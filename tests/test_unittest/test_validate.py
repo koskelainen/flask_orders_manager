21162cb8ee8d48b29ea514de5640253bfcfdce89
Evgenii Koskelainen <koskelainen8@gmail.com>
2024-03-15 18:33:37 +0100

    small fix

--- tests/test_unittest/test_validate.py
+++ tests/test_unittest/test_validate.py
@@ -20,68 +20,64 @@ class OrderFormTest(TestCase):
     def tearDown(self):
         inject.clear()
 
-    def _get_non_negative_length_plus_step(self, length, step=-1):
+    @staticmethod
+    def _get_non_negative_length_plus_step(length, step=-1):
         _len = length + step
         if _len < 0:
             return 0
         return _len
 
+    @staticmethod
+    def get_dummy_order_form_data(order_data: dict) -> OrderForm:
+        return OrderForm(DummyOrderData(order_data))
+
     def test_validate_success_order(self):
-        data = DummyOrderData({"name": "example_name", "address": "example_address"})
-        form = OrderForm(data)
+        form = self.get_dummy_order_form_data({"name": "example_name", "address": "example_address"})
         self.assertTrue(form.validate())
 
     def test_validate_invalid_order_small_name(self):
-        data = DummyOrderData({
+        form = self.get_dummy_order_form_data({
             "name": "".join(["a"] * self._get_non_negative_length_plus_step(NAME_MIN_LENGTH)),
             "address": "example_address",
         })
-        form = OrderForm(data)
         self.assertFalse(form.validate())
 
     def test_validate_invalid_order_small_address(self):
-        data = DummyOrderData({
+        form = self.get_dummy_order_form_data({
             "name": "example_name",
             "address": "".join(["a"] * self._get_non_negative_length_plus_step(ADDRESS_MIN_LENGTH)),
         })
-        form = OrderForm(data)
         self.assertFalse(form.validate())
 
     def test_validate_invalid_order_big_name(self):
-        data = DummyOrderData({
+        form = self.get_dummy_order_form_data({
             "name": "".join(["a"] * self._get_non_negative_length_plus_step(NAME_MAX_LENGTH, 1)),
             "address": "example_address",
         })
-        form = OrderForm(data)
         self.assertFalse(form.validate())
 
     def test_validate_invalid_order_big_address(self):
-        data = DummyOrderData({
+        form = self.get_dummy_order_form_data({
             "name": "example_name",
             "address": "".join(["a"] * self._get_non_negative_length_plus_step(ADDRESS_MAX_LENGTH, 1)),
         })
-        form = OrderForm(data)
         self.assertFalse(form.validate())
 
     def test_validate_invalid_order(self):
-        data = DummyOrderData({
+        form = self.get_dummy_order_form_data({
             "name": "".join(["a"] * self._get_non_negative_length_plus_step(NAME_MIN_LENGTH)),
             "address": "".join(["a"] * self._get_non_negative_length_plus_step(ADDRESS_MIN_LENGTH)),
         })
-        form = OrderForm(data)
         self.assertFalse(form.validate())
 
     def test_validate_invalid_order_empty(self):
-        data = DummyOrderData({"name": "", "address": ""})
-        form = OrderForm(data)
+        form = self.get_dummy_order_form_data({"name": "", "address": ""})
         self.assertFalse(form.validate())
 
     def test_validate_invalid_order_empty_name(self):
-        data = DummyOrderData({"name": "", "address": "example_address"})
-        form = OrderForm(data)
+        form = self.get_dummy_order_form_data({"name": "", "address": "example_address"})
         self.assertFalse(form.validate())
 
     def test_validate_invalid_order_empty_address(self):
-        data = DummyOrderData({"name": "example_name", "address": ""})
-        form = OrderForm(data)
+        form = self.get_dummy_order_form_data({"name": "example_name", "address": ""})
         self.assertFalse(form.validate())
