e8c2c90ee6cc2fe15eb61737d0a30865cc631e74
Evgenii Koskelainen <koskelainen8@gmail.com>
2024-03-17 18:35:10 +0100

    small review tests

--- tests/test_mamba/test_adapter/test_app.py
+++ tests/test_mamba/test_adapter/test_app.py
@@ -1,12 +1,15 @@
-from unittest.mock import Mock, patch
+from datetime import datetime
+from unittest.mock import Mock
 from urllib.parse import urljoin
 
 import inject
 from expects import be_none, be_true, contain, equal, expect, have_key
 from flask import url_for
 from jinja2 import TemplateNotFound
-from mamba import after, before, context, description, it, _it
+from mamba import after, before, context, description, it
 
+from src.adapter.database.database import db
+from src.adapter.models.models import OrderModel
 from tests.conftest import create_app
 
 with description("Test the Flask app by") as self:
@@ -28,10 +31,20 @@ with description("Test the Flask app by") as self:
     with context("launching it and verifying that"):
         with before.all:
             self.app = create_app()
+            self.order_1 = OrderModel(id=1,
+                                      name="Jon Snow",
+                                      address="22781 Charles Shores Heathertown",
+                                      created_at=datetime.now(),
+                                      updated_at=datetime.now())
+            self.order_2 = OrderModel(id=2,
+                                      name="Timothy Jones",
+                                      address="938 Bethany Course Suite 556 North Paul",
+                                      created_at=datetime.now(),
+                                      updated_at=datetime.now())
+            db.session = Mock(name="session")
             self.server_name = self.app.config.get("SERVER_NAME")
 
         with before.each:
-            self.store = Mock()
             self.client = self.app.test_client()
 
         with after.each:
@@ -66,56 +79,33 @@ with description("Test the Flask app by") as self:
                 expect(self.valid_status_codes).to(contain(response.status_code))
                 expect(response.location).to(equal(expected_location))
 
-            with _it("POST request with valid data to the 'success' page."):
-                form = {
-                    "name": "fake",
-                    "address": "fake-address",
-                }
-                expected_location = urljoin(f"http://{self.server_name}", "/orders/success")
-
-                response = self.client.post(url_for("orders.order_create"), data=form)
-                response = self.rebuild_response_location(response)
-                expect(self.valid_status_codes).to(contain(response.status_code))
-                expect(response.location).to(equal(expected_location))
-
-        with _it("GET request to the order by id which does not exist."):
-            order_id = 1234567
-            response = self.client.get(f"/orders/{order_id}/detail")
-            data = response.data.decode("utf-8")
-            expect(response.status_code).to(equal(404))
-            expect(data).to(contain("Not Found"))
-
         with it("was used template order list."):
+            query = Mock(name="query")
+            query.return_value = query
+            db.session.query = query
+            query.all.return_value = [self.order_1, self.order_2]
             self.client.get(url_for("orders.order_list"))
             self.assert_template_used("order_list.html")
 
         with it("was used template create list."):
+            db.session.query().filter_by(order_id=1).first.return_value = self.order_1
             self.client.get(url_for("orders.order_create"))
             self.assert_template_used("order_create.html")
 
         with it("was used template order detail."):
+            db.session.query().filter_by(order_id=1).first.return_value = self.order_1
             self.client.get(url_for("orders.order_detail", order_id=1))
             self.assert_template_used("order_detail.html")
 
         with it("was used template order update."):
-            self.client.get(url_for("orders.order_update", order_id=1))
-            self.assert_template_used("order_update.html")
-
-        with it("was used template order update."):
+            db.session.query().filter_by(order_id=1).first.return_value = self.order_1
             self.client.get(url_for("orders.order_update", order_id=1))
             self.assert_template_used("order_update.html")
 
         with it("was used template order delete."):
+            db.session.query().filter_by(order_id=1).first.return_value = self.order_1
             self.client.get(url_for("orders.order_delete", order_id=1))
             self.assert_template_used("order_delete.html")
-
-        with _it("GET request to the order list."):
-            expected_location = urljoin(f"http://{self.server_name}", None)
-            valid_status_codes = [200]
-            response = self.client.get(url_for("orders.order_list"))
-            response = self.rebuild_response_location(response)
-            expect(valid_status_codes).to(contain(response.status_code))
-            expect(response.location).to(equal(expected_location))
 
         with it("POST request to the order list."):
             expected_location = urljoin(f"http://{self.server_name}", None)
