2457de1f9bb656d4c2d9b93d80bae307e73368d4
Evgenii Koskelainen <koskelainen8@gmail.com>
2024-03-17 18:40:42 +0100

    fix quotes

--- tests/test_mamba/test_application/test_app.py
+++ tests/test_mamba/test_application/test_app.py
@@ -14,29 +14,29 @@ with description("Test the core logic application by") as self:
             self.app = create_app()
             self.store = Order()
             self.mock_response = Mock()
-            self.order = BaseOrderModel(name='Test Order', address='Test Address')
+            self.order = BaseOrderModel(name="Test Order", address="Test Address")
 
         with after.all:
             inject.clear()
 
         with it("check used method get_order"):
-            with patch.object(Order, 'get_order', return_value=self.mock_response) as mock_method:
+            with patch.object(Order, "get_order", return_value=self.mock_response) as mock_method:
                 response = self.store.get_order(order_id=1)
                 mock_method.assert_called_once_with(order_id=1)
                 expect(response).to(equal(self.mock_response))
 
         with it("check used method create_order"):
-            with patch.object(Order, 'create_order', return_value=None) as mock_method:
+            with patch.object(Order, "create_order", return_value=None) as mock_method:
                 self.store.create_order(order=self.order)
                 mock_method.assert_called_once_with(order=self.order)
 
         with it("check used method delete_order"):
-            with patch.object(Order, 'delete_order', return_value=None) as mock_method:
+            with patch.object(Order, "delete_order", return_value=None) as mock_method:
                 self.store.delete_order(order=self.order)
                 mock_method.assert_called_once_with(order=self.order)
 
         with it("check used method update_order"):
-            with patch.object(Order, 'update_order', return_value=self.mock_response) as mock_method:
+            with patch.object(Order, "update_order", return_value=self.mock_response) as mock_method:
                 response = self.store.update_order(order=self.order)
                 mock_method.assert_called_once_with(order=self.order)
                 expect(response).to(equal(self.mock_response))
